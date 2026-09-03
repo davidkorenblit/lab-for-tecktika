4e3ff9227bae9a30d10650d8db1cedf15c4167ae
Daniel Aroll <548408148d@gmail.com>
2026-09-03 14:06:33 +0300

    feat: add Azure queue client

--- backend/app/azure_clients.py
+++ backend/app/azure_clients.py
@@ -1,5 +1,6 @@
 from azure.identity import DefaultAzureCredential
 from azure.data.tables import TableServiceClient
+from azure.storage.queue import QueueServiceClient
 
 from app.core.config import settings
 
@@ -14,5 +15,16 @@ def get_table_service_client() -> TableServiceClient:
 
     return TableServiceClient(
         endpoint=account_url,
+        credential=credential,
+    )
+
+
+def get_queue_service_client() -> QueueServiceClient:
+    account_url = (
+        f"https://{settings.azure_storage_account_name}.queue.core.windows.net"
+    )
+
+    return QueueServiceClient(
+        account_url=account_url,
         credential=credential,
     )
